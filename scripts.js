87f50a9dbcf4ba01c7124c973bd8e591c71de615
Abby Kdd <ayqolpan.kdd@gmail.com>
2020-10-04 10:30:15 -0400

    Svae page up and running

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+
+let isOnSearch = true;
+let bookListName = ".search-books-list"
 var Book = Backbone.Model.extend({
 	defaults: {
 		id: '',
@@ -25,9 +28,20 @@ var BookView = Backbone.View.extend({
 	},
 
 	saveBook: function(){
-		var savedBooks = JSON.parse(localStorage.getItem("books")) || [];
-		savedBooks.push(this.model.attributes);
-		localStorage.setItem("books", JSON.stringify(savedBooks));
+		if(isOnSearch){
+			var savedBooks = JSON.parse(localStorage.getItem("books")) || [];
+			savedBooks.push(this.model.attributes);
+			localStorage.setItem("books", JSON.stringify(savedBooks));
+		}else{
+			var self = this;
+			var savedBooks = JSON.parse(localStorage.getItem("books"));
+			_.each(savedBooks, function(){
+				if (this.id == self.id){
+					savedBooks.pop(this);
+				}
+			});
+			localStorage.setItem("books", JSON.stringify(savedBooks));
+		}
 		this.remove();
 
 	},
@@ -44,15 +58,19 @@ var BookView = Backbone.View.extend({
 
 var BooksView = Backbone.View.extend({
 	model: books,
-	el: $('.books-list'),
+	el: $(bookListName),
 	initialize: function(){
 		this.model.on('add', this.render, this);
 	},
 	render: function(){
 		var self = this;
 		this.$el.html('');
+		this.$el = $(bookListName);
 		_.each(this.model.toArray(), function(book){
 			self.$el.append((new BookView({model: book})).render().$el);
+			if(!isOnSearch){
+				self.$('.save-book').html("Delete");
+			}
 		});
 		return this;
 	},
@@ -105,5 +123,30 @@ var getBooks = function(keyword){
 $(document).ready(function(){
 	$('.search').on('click', function(){
 		getBooks($('.search-input').val());
+	});
+	$('.save-page-link').on('click', function(){
+		isOnSearch = false;
+		bookListName = ".save-books-list";
+		$('.search-page').hide();
+		$('.save-page').show();
+		$('.warning').hide();
+		var savedBooks = JSON.parse(localStorage.getItem("books")) || [];
+		if(savedBooks.length == 0){
+			$('.warning').show();
+		}else{
+			$('.warning').hide();
+			books.reset();
+			_.each(savedBooks, function(data){
+				var obj = new Book(data);
+				books.add(obj);
+			});
+			booksView.render();
+		}
+	});
+	$('.search-page-link').on('click', function(){
+		isOnSearch = true;
+		bookListName = ".search-books-list";
+		$('.search-page').show();
+		$('.save-page').hide();
 	});
 })
