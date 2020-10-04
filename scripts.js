7bb05b60345adbc68ac64e00c50ee4b878c13ab6
Abby Kdd <ayqolpan.kdd@gmail.com>
2020-10-04 09:58:44 -0400

    working save btn save books to the localstorage

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-
 var Book = Backbone.Model.extend({
 	defaults: {
+		id: '',
 		title: '',
 		author: '',
 		year: '',
@@ -10,6 +10,7 @@ var Book = Backbone.Model.extend({
 		readerLink: '',
 	}
 });
+
 var Books = Backbone.Collection.extend({});
 
 var books = new Books();
@@ -17,21 +18,18 @@ var books = new Books();
 var BookView = Backbone.View.extend({
 	model: new Book(),
 	tagName: 'div',
-	className: 'col',
+	className: 'col s12 m7 book-container',
 
-	// events:{
-	// 	'mouseover': "enter",
-	// 	'mouseout': "leave",
-	// },
-
-	enter: function(e){
-		this.$('.book-container').addClass('overlay-img');
-		this.$('.btn-container').show();
+	events: {
+		'click .save-book': 'saveBook',
 	},
 
-	leave: function(e){
-		this.$('.book-container').removeClass('overlay-img');
-		this.$('.btn-container').hide();
+	saveBook: function(){
+		var savedBooks = JSON.parse(localStorage.getItem("books")) || [];
+		savedBooks.push(this.model.attributes);
+		localStorage.setItem("books", JSON.stringify(savedBooks));
+		this.remove();
+
 	},
 
 	initialize: function(){
@@ -60,41 +58,48 @@ var BooksView = Backbone.View.extend({
 	},
 });
 
+
+
 var booksView = new BooksView();
 
 var getBooks = function(keyword){
-	var BookCollection = Backbone.Collection.extend({
-		url: `https://www.googleapis.com/books/v1/volumes?q=${keyword}`
-	});
-	books.reset();
-	var collection = new BookCollection();
-	collection.fetch({
-		success: function(){
-			var list = collection.models[0].attributes.items;
-			_.each(list, function(cur){
-				var des = cur.volumeInfo.subtitle;
-				if(!des){
-					des = "";
-				}
-				var authors = cur.volumeInfo.authors;
-				var mainAuthor = '';
-				if (authors){
-					mainAuthor = authors[0];
-				}
-				var book = new Book({
-					title: cur.volumeInfo.title.substring(0,50),
-					author: mainAuthor,
-					year: cur.volumeInfo.publishedDate,
-					imgUrl: cur.volumeInfo.imageLinks.thumbnail,
-					language: cur.volumeInfo.language,
-					description: des.substring(0, 100),
-					readerLink: cur.volumeInfo.infoLink,
+	if(!keyword || keyword == ' '){
+		$('.err').show();
+	}else{
+		$('.err').hide();
+		var BookCollection = Backbone.Collection.extend({
+			url: `https://www.googleapis.com/books/v1/volumes?q=${keyword}`
+		});
+		books.reset();
+		var collection = new BookCollection();
+		collection.fetch({
+			success: function(){
+				var list = collection.models[0].attributes.items;
+				_.each(list, function(cur){
+					var des = cur.volumeInfo.subtitle;
+					if(!des){
+						des = "";
+					}
+					var authors = cur.volumeInfo.authors;
+					var mainAuthor = '';
+					if (authors){
+						mainAuthor = authors[0];
+					}
+					var book = new Book({
+						id: cur.id,
+						title: cur.volumeInfo.title.substring(0,50),
+						author: mainAuthor,
+						year: cur.volumeInfo.publishedDate,
+						imgUrl: cur.volumeInfo.imageLinks.thumbnail,
+						language: cur.volumeInfo.language,
+						description: des,
+						readerLink: cur.volumeInfo.infoLink,
+					});
+					books.add(book);
 				});
-				books.add(book);
-			});
-		}
-	});
-	$('.search-input').val('');
+			}
+		});
+	}
 }
 
 $(document).ready(function(){
